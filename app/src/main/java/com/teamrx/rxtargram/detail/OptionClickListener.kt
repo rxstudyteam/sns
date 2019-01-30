9943df1c3001246a9c6952de6dbdd7c3985dabca
렐 <karrel.kor@gmail.com>
2019-01-30 22:43:33 +0900

    test push

--- app/src/main/java/com/teamrx/rxtargram/detail/OptionClickListener.kt
+++ app/src/main/java/com/teamrx/rxtargram/detail/OptionClickListener.kt
@@ -2,7 +2,7 @@ package com.teamrx.rxtargram.detail
 
 import com.teamrx.rxtargram.model.Post
 
-interface OptionClickListener { 
+interface OptionClickListener {
     fun onOptionClick(post: Post?)
 
 }
