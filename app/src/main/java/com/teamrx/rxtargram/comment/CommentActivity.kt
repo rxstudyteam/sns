69a3a0722b3040a08a9838e38d62343dfcb1d72e
김지훈 <rlawlgns077@gmail.com>
2019-02-15 05:34:37 +0000

    댓글 보기 + 쓰기 UI 수정중.
    PullToRefreshLayout 적용과  RecyclerView ViewType 분리 고민해보기

---
+++ app/src/main/java/com/teamrx/rxtargram/comment/CommentActivity.kt
@@ -0,0 +1,119 @@
+package com.teamrx.rxtargram.comment
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.teamrx.rxtargram.R
+import com.teamrx.rxtargram.base.BaseViewModel
+import com.teamrx.rxtargram.detail.DetailViewModel
+import com.teamrx.rxtargram.inject.Injection
+import com.teamrx.rxtargram.model.CommentDTO
+import com.teamrx.rxtargram.model.Post
+import kotlinx.android.synthetic.main.activity_comment.*
+
+class CommentActivity : AppCompatActivity() {
+
+    private lateinit var commentViewModel: CommentViewModel
+    private lateinit var detailViewModel: DetailViewModel
+    private lateinit var adapter: CommentAdapter
+
+    private var postId: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+        setSupportActionBar(toolBar)
+
+        supportActionBar?.title = getString(R.string.comment)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val arguments = intent.extras
+        postId = arguments?.getString("post_id")
+
+        setupRecyclerView()
+        setupEvent()
+        setupViewModel()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = CommentAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@CommentActivity)
+    }
+
+    private fun setupEvent() {
+        tvSummit.setOnClickListener {
+            // 로그인상태가 아니므로 임의의 사용자
+            val user_id = "MrTiDrASkFH9hby1x9VD"
+            commentViewModel.addComment(postId.toString(), user_id, edtContent.text.toString()) { isSuccess ->
+                if(isSuccess) {
+                    edtContent.setText("")
+                    Toast.makeText(this, getString(R.string.comment_add_success), Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.comment_add_failed), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun setupViewModel() {
+        detailViewModel = getViewModel()
+        commentViewModel = getViewModel()
+
+        detailViewModel.getPostById().observe(this, Observer { post ->
+            updatePost(post)
+        })
+
+        commentViewModel.getComments().observe(this, Observer { comments ->
+            updateComments(comments)
+        })
+
+        detailViewModel.loadPostById(postId.toString())
+        commentViewModel.loadComments(postId.toString())
+    }
+
+    private fun updatePost(post: Post) {
+        tvUserId.text = post.user_id
+        tvContent.text = post.content
+        tvCreatedAt.text = post.created_at?.toDate().toString()
+    }
+
+    private fun updateComments(comments: List<CommentDTO>) {
+        adapter.setComments(comments)
+    }
+
+    private inline fun <reified T : BaseViewModel> getViewModel(): T {
+        val viewModelFactory = Injection.provideViewModelFactory()
+        return ViewModelProviders.of(this, viewModelFactory).get(T::class.java)
+    }
+
+    companion object {
+        fun startActivity(activity: Activity, post_id: String) {
+            val intent = Intent(activity, CommentActivity::class.java)
+            val bundle = Bundle()
+            bundle.putString("post_id", post_id)
+
+            intent.putExtras(bundle)
+
+            activity.startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/teamrx/rxtargram/comment/CommentViewModel.kt
@@ -0,0 +1,24 @@
+package com.teamrx.rxtargram.comment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.teamrx.rxtargram.base.BaseViewModel
+import com.teamrx.rxtargram.model.CommentDTO
+import com.teamrx.rxtargram.repository.AppDataSource
+
+class CommentViewModel(appDataSource: AppDataSource): BaseViewModel(appDataSource) {
+
+    private val commentDTOLiveData: MutableLiveData<List<CommentDTO>> by lazy { MutableLiveData<List<CommentDTO>>() }
+
+    fun getComments(): LiveData<List<CommentDTO>> = commentDTOLiveData
+    fun loadComments(post_id: String) {
+        dataSource.getComments(post_id) { comments ->
+            commentDTOLiveData.value = comments
+        }
+    }
+
+    fun addComment(parent_post_id: String, user_id: String, content: String, callback: (Boolean) -> Unit) {
+        dataSource.addComment(parent_post_id, user_id, content, callback)
+    }
+
+}
