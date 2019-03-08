457199c4606378c792aa2509ec486969c65b9876
karrel <karrel84@naver.com>
2019-03-08 18:16:13 +0900

    가입 액티비티 추가

---
+++ app/src/main/java/com/teamrx/rxtargram/join/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.teamrx.rxtargram.join
+
+import com.teamrx.rxtargram.base.AppActivity
+
+/**
+ * Created by Rell on 2019. 3. 8..
+ * 회원 가입 액티비티
+ */
+class JoinActivity : AppActivity() {
+
+
+}
