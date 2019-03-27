7ff6b712f2efe2960c93913790aeab75d640f6d0
eastarj <eastarj@gmail.com>
2019-03-28 04:09:44 +0900

    카카오 hash 키 생성  pie 대응

--- app/src/main/java/com/teamrx/rxtargram/base/AppApplication.kt
+++ app/src/main/java/com/teamrx/rxtargram/base/AppApplication.kt
@@ -1,7 +1,9 @@
 package com.teamrx.rxtargram.base
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Base64
 import smart.base.BApplication
 import java.security.MessageDigest
@@ -15,18 +17,26 @@ class AppApplication : BApplication() {
 
     fun hashkeyForDaumMap(context: Context) {
         try {
-            val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
-            for (signature in info.signatures) {
+            if (Build.VERSION.SDK_INT >= 28) {
+                @SuppressLint("WrongConstant")
+                val packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
                 val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                android.util.Log.e(context.packageName, Base64.encodeToString(md.digest(), Base64.NO_WRAP))
+                for (signature in packageInfo.signingInfo.apkContentsSigners) {
+                    md.update(signature.toByteArray())
+                    android.util.Log.e(context.packageName, Base64.encodeToString(md.digest(), Base64.NO_WRAP))
+                }
+            } else {
+                val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+                val md = MessageDigest.getInstance("SHA")
+                for (signature in info.signatures) {
+                    md.update(signature.toByteArray())
+                    android.util.Log.e(context.packageName, Base64.encodeToString(md.digest(), Base64.NO_WRAP))
+                }
             }
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         } catch (e: NoSuchAlgorithmException) {
             e.printStackTrace()
         }
-
     }
-
 }
