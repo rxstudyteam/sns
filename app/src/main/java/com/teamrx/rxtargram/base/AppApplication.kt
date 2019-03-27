0287c06281e92ad2341d4fa65bcd0fed6250d4aa
eastarj <eastarj@gmail.com>
2019-03-28 03:51:49 +0900

    카카오 api 키 해쉬추가

--- app/src/main/java/com/teamrx/rxtargram/base/AppApplication.kt
+++ app/src/main/java/com/teamrx/rxtargram/base/AppApplication.kt
@@ -1,5 +1,32 @@
 package com.teamrx.rxtargram.base
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Base64
 import smart.base.BApplication
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
-class AppApplication : BApplication()
+class AppApplication : BApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        hashkeyForDaumMap(applicationContext)
+    }
+
+    fun hashkeyForDaumMap(context: Context) {
+        try {
+            val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                android.util.Log.e(context.packageName, Base64.encodeToString(md.digest(), Base64.NO_WRAP))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+
+    }
+
+}
